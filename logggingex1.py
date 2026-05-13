bb1bcac7dc0e423099a82910225949f888891edc
Manikandan <amanikandan79@gmail.com>
2026-05-13 16:41:10 +0800

    Logging ex1

---
+++ logggingex1.py
@@ -0,0 +1,5 @@
+import logging
+logging.basicConfig(filename='C:\\manilogs\\example.log', level=logging.INFO)
+logging.info("This is an info message")
+logging.warning("This is a warning message")
+logging.error("This is an error message")
